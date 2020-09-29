1863432102073ea9f5452312b6a46458737eef67
Sanjaydotcom <nssanjay7@gmail.com>
2020-09-29 23:54:18 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import SearchPage from "./Views/SearchPage"
 import * as serviceWorker from './serviceWorker';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#ffa73c",
+    },
+
+  },
+});
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+    <SearchPage />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
